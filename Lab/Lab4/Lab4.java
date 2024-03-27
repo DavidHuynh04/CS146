3f33e251ad625b534470f5961e024b81040a2959
DavidHuynh04 <131493560+DavidHuynh04@users.noreply.github.com>
2024-03-26 20:31:59 -0700

    Add files via upload

---
+++ Lab/Lab4/Lab4.java
@@ -0,0 +1,17 @@
+public class Lab4 {
+    public TreeNode invertTree(TreeNode root) {
+            if (root == null){
+                return root;
+            }
+            TreeNode temp = root.left;
+            root.left = root.right;
+            root.right = temp;
+            if (root.left != null){
+                invertTree(root.left);
+            }
+            if (root.right != null){
+                invertTree(root.right);
+            }
+            return root;
+    }
+}
